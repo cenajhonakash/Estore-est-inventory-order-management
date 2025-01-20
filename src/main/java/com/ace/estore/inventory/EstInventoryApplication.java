d4c4ead4ed94bc5b1d369149ecf29c41c7446854
Jyoti <jyoti.sdet11@gmail.com>
2025-01-20 11:07:21 +0530

    Removed security configuration & used est-common jar

--- src/main/java/com/ace/estore/inventory/EstInventoryApplication.java
+++ src/main/java/com/ace/estore/inventory/EstInventoryApplication.java
@@ -3,7 +3,7 @@ package com.ace.estore.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.ace.estore.common.config", "com.ace.estore.inventory" })
 public class EstInventoryApplication {
 
 	public static void main(String[] args) {
